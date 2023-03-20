c9d27ca2febdf5d51c8b338bf2ce6d4dfec68e1d
이재성 <wlstk218@naver.com>
2023-03-20 14:10:08 +0900

    commitA

---
+++ FAF_3110/src/main/java/com/example/faf_3110/Faf3110Application.java
@@ -0,0 +1,13 @@
+package com.example.faf_3110;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Faf3110Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Faf3110Application.class, args);
+	}
+
+}
---
+++ FAF_3110/src/test/java/com/example/faf_3110/Faf3110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faf_3110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Faf3110ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
